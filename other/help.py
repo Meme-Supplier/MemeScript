0ec96ddce0c06c72511e4262638bc12586360463
Meme Guy <owen.debiasio@gmail.com>
2024-12-23 18:18:18 -0500

    Update help.py
    Lists new syntax that was added

--- other/help.py
+++ other/help.py
@@ -2,9 +2,13 @@ print()
 
 print("Available features:\n")
 
-print("Output some text:           output(string)")
-print("User input:                 ask(string)")
-print("Assign a variable:          assign(<variable name> , <value>) MUST BE EXACTLY LIKE THIS, AND ONE VARIABLE AT A TIME")
-print("Clear screen:               varout(variable)")
+print("Output some text:                        output(string)")
+print("Output the variable:                     varout()")
+print("User input:                              ask(string)")
+print("Variable set by user input:              askvar(string)")
+print("Assign a value to the variable:          assign(<variable name> , <value>) MUST BE EXACTLY LIKE THIS, AND ONE VARIABLE AT A TIME")
+print("Clear screen:                            cls()")
+print("Generate a random number (0 , max):      rand(maximum number)")
+print("Assign a random number to a variable:    randvar(<variable> , <maximum number>) MUST BE EXACTLY LIKE THIS")
 
 print()
